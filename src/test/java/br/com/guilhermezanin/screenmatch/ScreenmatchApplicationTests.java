0a6b71bfa6c8edc7a141eae7cca90ff9d0bbc959
Guilherme <guilhermecasazanin@gmail.com>
2024-01-05 14:28:51 -0300

    first commit

---
+++ src/test/java/br/com/guilhermezanin/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermezanin.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
