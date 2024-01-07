d52bde8db24c301b7ce16643894059ec3680e4c4
Guilherme <guilhermecasazanin@gmail.com>
2024-01-07 08:52:28 -0300

    desenvolvimento serie e episodios

--- src/main/java/br/com/guilhermezanin/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/guilhermezanin/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,9 @@
 package br.com.guilhermezanin.screenmatch;
 
+import br.com.guilhermezanin.screenmatch.model.ConverterDados;
+import br.com.guilhermezanin.screenmatch.model.DadosEpisodio;
+import br.com.guilhermezanin.screenmatch.model.DadosSerie;
+import br.com.guilhermezanin.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +17,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("auiiiiii");
+		String endereco = "https://www.omdbapi.com/?apikey=a5a0bf5&t=Gilmore+girls&season=1&episode=2";
+
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados(endereco);
+
+		ConverterDados converterDados = new ConverterDados();
+
+		//DadosSerie dadosSerie = converterDados.obterDados(json, DadosSerie.class);
+		DadosEpisodio dadosEpisodio = converterDados.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 }
